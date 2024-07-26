70df86853bee2fdb55042ea1d7cbca86868f7ef9
Noah Pavuk <pavuk.10@osu.edu>
2024-07-26 11:24:16 -0400

    Added MVAPICH logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path   = ['_static']
+html_logo          = '_static/MVAPICH_logo.jpg'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
